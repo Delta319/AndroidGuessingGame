1c68f3777d765acc839183206ed819721f4cc633
Ethan Rowe <redsoxrowe@gmail.com>
2018-06-15 14:37:09 -0400

    Added case to protect from invalid number

--- app/src/main/java/com/example/ethanrowe/guessinggame/GameActivity.java
+++ app/src/main/java/com/example/ethanrowe/guessinggame/GameActivity.java
@@ -1,13 +1,74 @@
 package com.example.ethanrowe.guessinggame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class GameActivity extends AppCompatActivity {
+
+    private Button guessButton;
+    private TextView clue;
+    private EditText guess;
+
+    private int generatedNumber;
+    private int numberOfGuesses = 0;
+    private final int MAX_GUESS_COUNT = 4;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        guessButton = findViewById(R.id.submit_guess_button);
+        clue = findViewById(R.id.clue);
+        guess = findViewById(R.id.guess_edittext);
+
+        //This generates a random number between 1 and 100.
+        generatedNumber = (int) Math.ceil(Math.random() * 100);
+
+        //Toast.makeText(this, Integer.toString(generatedNumber), Toast.LENGTH_SHORT).show();
+
+        setListener();
+
+    }
+
+    private void setListener() {
+        guessButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validateGuess();
+            }
+        });
+    }
+
+    //This method is going to check if the user has put in a valid number/
+    private void validateGuess() {
+
+
+        int userGuess = Integer.parseInt(guess.getText().toString());
+        if (userGuess > 100 || userGuess <= 0) {
+            clue.setText("Please enter a number between 1 and 100.");
+            clue.setVisibility(View.VISIBLE);
+            guess.setText("");
+        } else {
+            checkGuess(userGuess);
+        }
+    }
+
+    //This method will take the input and check it against the generatedNumber. Depending on the outcome it will change the view accordingly or take us to the ResultsActivity;
+    private void checkGuess(int userGuess) {
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        //Removed super.onBackPressed(); to make sure if the back button is presses nothing will happen.
     }
 }
